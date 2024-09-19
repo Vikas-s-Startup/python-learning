ccb444b5820da57501d27200bf6d8379fd955e00
Vikas Koppineedi <vikaskoppineedi@gmail.com>
2024-09-19 07:05:01 -0400

    final program

--- Chapter-5/listing5.4.py
+++ Chapter-5/listing5.4.py
@@ -1,10 +1,13 @@
 import random
+import time
 
 NUMBER_OF_QUESTIONS = 5
 CURRENT_QUESTION = 0
 
 CORRECT_ANSWERS = 0
 WRONG_ANSWERS = 0
+
+start_time = time.time()
 
 while CURRENT_QUESTION < NUMBER_OF_QUESTIONS:
     number1 = random.randint(1,10)
@@ -19,9 +22,15 @@ while CURRENT_QUESTION < NUMBER_OF_QUESTIONS:
 
     if system_answer == user_answer:
         CORRECT_ANSWERS = CORRECT_ANSWERS + 1
+        print("your answer is correct \n")
     else:
         WRONG_ANSWERS = WRONG_ANSWERS + 1
+        print("Your answer is wrong \n")
 
     CURRENT_QUESTION = CURRENT_QUESTION + 1
 
-print(f"Correct answers are {CORRECT_ANSWERS} and wrong answers are {WRONG_ANSWERS}")
+end_time = time.time()
+
+time_taken = end_time-start_time
+
+print(f"\nCorrect answers are {CORRECT_ANSWERS} and wrong answers are {WRONG_ANSWERS} and time taken to answer is {time_taken}")
