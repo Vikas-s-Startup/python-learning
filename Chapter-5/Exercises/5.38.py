2b0ca3252f9703d0e15c34e9ded2fc65ee99eb74
vikas koppineedi <vikaskoppineedi@gmail.com>
2024-10-11 13:21:57 -0400

    Adding more programs

--- Chapter-5/Exercises/5.38.py
+++ Chapter-5/Exercises/5.38.py
@@ -1,22 +1,46 @@
-stringA = input("Enter a string A")
-stringB = input("Enter a string B")
+# # Longest common prefix
+# stringA = input("Enter a string A")
+# stringB = input("Enter a string B")
+#
+# common_prefix = ""
+# smallest_string = ""
+# largest_string = ""
+#
+# if len(stringB) < len(stringA):
+#     smallest_string = stringB
+#     largest_string = stringA
+# elif len(stringA) < len(stringB):
+#     smallest_string = stringA
+#     largest_string = stringB
+# elif len(stringB) == len(stringA):
+#     smallest_string = stringB
+#     largest_string = stringA
+#
+# for i in range(len(smallest_string)):
+#     if smallest_string[i] == largest_string[i]:
+#         common_prefix = common_prefix + smallest_string[i]
+#
+# print("Common Prefix", common_prefix)
 
-common_prefix = ""
-smallest_string = ""
-largest_string = ""
 
-if len(stringB) < len(stringA):
-    smallest_string = stringB
-    largest_string = stringA
-elif len(stringA) < len(stringB):
-    smallest_string = stringA
-    largest_string = stringB
-elif len(stringB) == len(stringA):
-    smallest_string = stringB
-    largest_string = stringA
+# Longest common prefix
+stringA = input("Enter a string A: ")
+stringB = input("Enter a string B: ")
 
-for i in range(len(smallest_string)):
-    if smallest_string[i] == largest_string[i]:
-        common_prefix = common_prefix + smallest_string[i]
+# Find the smaller length to avoid redundant comparisons
+min_length = min(len(stringA), len(stringB))
 
-print("Common Prefix", common_prefix)
+# Initialize list to collect common characters
+common_prefix_chars = []
+
+# Compare the characters of both strings up to the min_length
+for i in range(min_length):
+    if stringA[i] == stringB[i]:
+        common_prefix_chars.append(stringA[i])
+    else:
+        break  # Exit loop as soon as characters mismatch
+
+# Join the collected characters into a string
+common_prefix = ''.join(common_prefix_chars)
+
+print("Common Prefix:", common_prefix)
