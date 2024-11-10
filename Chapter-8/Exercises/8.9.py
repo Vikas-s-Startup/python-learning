9b3a0faa27d1358f75817a4ad7e74da92fa691de
Vikas Koppineedi <vikaskoppineedi@gmail.com>
2024-11-10 11:48:46 -0500

    adding new values

--- Chapter-8/Exercises/8.9.py
+++ Chapter-8/Exercises/8.9.py
@@ -30,50 +30,49 @@ def did_player_win(board, playerOption, posRow, posColumn):
 
 
 def main():
-    tic_tac_toe_board = [[" " for x in range(3)] for y in range(3)]
-    print("Empty Board")
-    print_tic_tac_toe_board(tic_tac_toe_board)
+	tic_tac_toe_board = [[" " for x in range(3)] for y in range(3)]
+	print("Empty Board")
+	print_tic_tac_toe_board(tic_tac_toe_board)
 
-    playerAOption = 'X'
-    playerBOption = '0'
+	playerAOption = 'X'
+	playerBOption = '0'
 
-    gameOver = False
+	gameOver = False
 
-    while not gameOver:
-        # Player A's turn
-        while True:
-            playerARow = int(input("Enter a row for player A (X): "))
-            playerAColumn = int(input("Enter a column for player A (X): "))
-            # Check if the cell is empty
-            if tic_tac_toe_board[playerARow][playerAColumn] == " ":
-                tic_tac_toe_board[playerARow][playerAColumn] = playerAOption
-                break  # Exit loop if position is valid
-            else:
-                print("Position already taken. Choose a different position.")
+	while not gameOver:
+		# Player A's turn
+		while True:
+			playerARow = int(input("Enter a row for player A (X): "))
+			playerAColumn = int(input("Enter a column for player A (X): "))
+			# Check if the cell is empty
+			if tic_tac_toe_board[playerARow][playerAColumn] == " ":
+				tic_tac_toe_board[playerARow][playerAColumn] = playerAOption
+				break  # Exit loop if position is valid
+			else:
+				print("Position already taken. Choose a different position.")
 
-        print_tic_tac_toe_board(tic_tac_toe_board)
+		print_tic_tac_toe_board(tic_tac_toe_board)
 
-        if did_player_win(tic_tac_toe_board, playerAOption, playerARow, playerAColumn):
-            print("Player A (X) won! Hurraaaay!")
-            break
+		if did_player_win(tic_tac_toe_board, playerAOption, playerARow, playerAColumn):
+			print("Player A (X) won! Hurraaaay!")
+			break
 
-        # Player B's turn
-        while True:
-            playerBRow = int(input("Enter a row for player B (0): "))
-            playerBColumn = int(input("Enter a column for player B (0): "))
-            # Check if the cell is empty
-            if tic_tac_toe_board[playerBRow][playerBColumn] == " ":
-                tic_tac_toe_board[playerBRow][playerBColumn] = playerBOption
-                break  # Exit loop if position is valid
-            else:
-                print("Position already taken. Choose a different position.")
+		# Player B's turn
+		while True:
+			playerBRow = int(input("Enter a row for player B (0): "))
+			playerBColumn = int(input("Enter a column for player B (0): "))
+			# Check if the cell is empty
+			if tic_tac_toe_board[playerBRow][playerBColumn] == " ":
+				tic_tac_toe_board[playerBRow][playerBColumn] = playerBOption
+				break  # Exit loop if position is valid
+			else:
+				print("Position already taken. Choose a different position.")
 
-        print_tic_tac_toe_board(tic_tac_toe_board)
+		print_tic_tac_toe_board(tic_tac_toe_board)
 
-        if did_player_win(tic_tac_toe_board, playerBOption, playerBRow, playerBColumn):
-            print("Player B (0) won! Hurraaaay!")
-            break
-
+		if did_player_win(tic_tac_toe_board, playerBOption, playerBRow, playerBColumn):
+			print("Player B (0) won! Hurraaaay!")
+			break
 
 
 main()
